884cdc1b6fe20dc79060351d36ea73256ca1d4cb
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-12-18 09:12:05 +0200

    initial project 5 files added

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstDemo.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {           
+            optionsBuilder.UseSqlServer("Server=.;Integrated Security=true; Database=CodeFirstDemo2021");
+        }
+        
+        public DbSet<Category> Categories { get; set; } // name of the DB TAble 
+        public DbSet<News> News { get; set; }//create models as objs and from them Tables
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using CodeFirstDemo.Models;
+using System;
+using System.Linq;
+
+namespace CodeFirstDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new ApplicationDbContext();
+            db.Database.EnsureCreated();
+        }
+    }
+}
